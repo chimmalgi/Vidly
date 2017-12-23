6dc6983aaeaf40d30e65b0394989df40dfd80a23
raj <raj@ZEROLC>
2017-12-23 00:26:05 -0800

    Initial

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        // GET: Movies/Details
+        public ActionResult Details(int Id)
+        {
+            var movie = GetMovies().SingleOrDefault(i => i.Id == Id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1", Id = 1 },
+                new Customer { Name = "Customer 2", Id = 2}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            
+            return View(viewModel);
+          
+        }
+
+
+    }
+}
